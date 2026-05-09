4036188761102f43ba264b3eea856776cf9efb07
Eugen Scutaru <evgensc5@gmail.com>
2026-05-09 11:46:18 +0300

    Add Living

--- src/Casa.java
+++ src/Casa.java
@@ -2,6 +2,7 @@ public class Casa {
     public static void main (String[] args){
         System.out.println("Bucatarie");
         System.out.println("Dormitor");
+        System.out.println("Living");
 
     }
 }
