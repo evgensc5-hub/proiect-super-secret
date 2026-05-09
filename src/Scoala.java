dae5b8a814da1077c67bd1f13f5377cf59582586
Sorin <soringoredev@gmail.com>
2026-05-09 12:01:00 +0300

    Add School

---
+++ src/Scoala.java
@@ -0,0 +1,9 @@
+public class Scoala {
+    public static void main(String[] args) {
+        System.out.println("Clasa1");
+        System.out.println("Clasa2");
+        System.out.println("Clasa3");
+        System.out.println("Clasa4");
+        System.out.println("Clasa5");
+    }
+}
