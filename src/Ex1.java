ca2fb4080758a8151eee85f92d1f40a6de72a72f
Ceban Valentina <cebanvalentina9@gamil.com>
2026-05-09 11:58:10 +0300

    Add Valentina

--- src/Ex1.java
+++ src/Ex1.java
@@ -2,6 +2,7 @@ public class Ex1 {
     public static void main(String[] args) {
         System.out.println("Sorin");
         System.out.println("Eugen");
+        System.out.println("Valentina");
     }
 
 
